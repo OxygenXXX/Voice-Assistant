33d91332aa6115136ca83e3f922a235481380107
OxygenXXX <oxyyygen.developer@gmail.com>
2023-01-05 18:38:24 +0000

    Initial commit

---
+++ assistant.py
@@ -0,0 +1,11 @@
+
+class VoiceAssistant:
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+
+    import config as va_config
+
+    import system.tts_system as tts_system
+    import system.stt_system as stt_system
---
+++ system/stt_system.py
@@ -0,0 +1,6 @@
+
+def import_message():
+    print(f"Subsystem ${__name__} imported successfully!")
+
+if __name__ != "__main__":
+    import_message()
